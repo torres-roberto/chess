87d5b12b73b4bfc163f44dc4f6d7a763c6db2e3a
torres-roberto <torres-roberto@outlook.com>
2019-04-08 22:38:17 -0500

    bishop can move!

--- src/app/rules/possible-moves/possibleMoves.ts
+++ src/app/rules/possible-moves/possibleMoves.ts
@@ -1,3 +1,4 @@
+import { BishopMoves } from './bishop-move';
 import { KnightMoves } from './knight-move';
 import { PawnMoves } from './pawn-move';
 import { Injectable } from '@angular/core';
@@ -7,7 +8,8 @@ import { Injectable } from '@angular/core';
 })
 export class PossibleMoves {
   constructor(private pawnMoves: PawnMoves,
-              private knightMoves: KnightMoves
+              private knightMoves: KnightMoves,
+              private bishopMoves: BishopMoves
   ) {}
 
   getPossibleMoves(chessPiece: ChessPiece): string[] {
@@ -16,6 +18,8 @@ export class PossibleMoves {
         return this.knightMoves.getPossibleMoves(chessPiece);
       case 'pawn':
         return this.pawnMoves.getPossibleMoves(chessPiece);
+      case 'bishop':
+        return this.bishopMoves.getPossibleMoves(chessPiece);
       default:
         console.log(`Invalid piece - Cannot get possible moves for piece of type [${chessPiece.name}]`);
         return null;
